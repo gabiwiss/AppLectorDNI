02c77f7d4e384a45d8595b7f237347c1a806ab6d
gabiwiss <gabrielwissler@gmail.com>
2023-11-17 16:57:23 -0300

    Agregado de model socio

--- AppLectorDNI/Models/Socio.cs
+++ AppLectorDNI/Models/Socio.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace AppLectorDNI.Models
 {
-    internal class Socio
+    public class Socio
     {
+        public Socio() { }
+
+        public Socio(string[] datosSocio)
+        {
+            Numero = Convert.ToInt32(datosSocio[0]);
+            Apellido = datosSocio[1];
+            Nombre = datosSocio[2];
+            Genero = datosSocio[3];
+            DNI = Convert.ToInt32(datosSocio[4]);
+            Ejemplar = datosSocio[5];
+            FechaNacimiento = DateOnly.Parse(datosSocio[6]);
+            FechaDeEmision = DateOnly.Parse(datosSocio[7]);
+            Numero2 = Convert.ToInt32(datosSocio[8]);
+        }
+        
+        private int Numero { get; }
+        private string Apellido { get; set; }
+        private string Nombre { get; set; }
+        private string Genero { get; set; }
+        private int DNI { get; set; }
+        private string Ejemplar { get; set; }
+        private DateOnly FechaNacimiento { get; set; }
+        private DateOnly FechaDeEmision { get; set; }
+        private int Numero2 { get; set; }
+
+        public int ObtenerDNI()
+        {
+            return DNI; 
+        }
     }
 }
