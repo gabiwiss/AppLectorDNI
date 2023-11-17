700ef7c1c50c4321c1cd97c9b40771d0bc486336
gabiwiss <gabrielwissler@gmail.com>
2023-11-17 17:00:07 -0300

    Agregada clase que busca un string dado dentro de un archivo pdf

---
+++ AppLectorDNI/Services/BuscarEnPdf.cs
@@ -0,0 +1,42 @@
+﻿using iText;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PdfSharp.Snippets.Drawing.ImageHelper;
+
+namespace AppLectorDNI.Services
+{
+    public class BuscarEnPdf
+    {
+        public static bool searchForText(string path, string dni)
+        {
+            PdfReader pdfReader = new PdfReader(path);
+            PdfDocument pdfParser = new PdfDocument(pdfReader);
+
+            // extract the text
+            var text =  ExtractTextFromPdf(pdfParser);
+
+            return text.Contains(dni);   
+        }
+
+        public static string ExtractTextFromPdf(PdfDocument pdfDoc)
+        {
+            StringWriter output = new StringWriter();
+
+            for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+            {
+                PdfPage page = pdfDoc.GetPage(i);
+                ITextExtractionStrategy strategy = new LocationTextExtractionStrategy();
+                string text = PdfTextExtractor.GetTextFromPage(page, strategy);
+                output.WriteLine(text);
+            }
+
+            return output.ToString();
+        }
+    }
+}
