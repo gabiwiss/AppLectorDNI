b4072bbe67034e3b04e467042b924ce8a5e74a21
gabiwiss <gabrielwissler@gmail.com>
2023-11-19 20:26:03 -0300

    Agregado temporizador para desactivar label (mensaje de respuesta)

--- AppLectorDNI/Form1.cs
+++ AppLectorDNI/Form1.cs
@@ -1,14 +1,18 @@
 using AppLectorDNI.Models;
 using AppLectorDNI.Services;
+using System.Windows.Forms;
 namespace AppLectorDNI
 {
     public partial class Form1 : Form
     {
         private readonly TextoPDF textoPDF;
+        private System.Windows.Forms.Timer timer;
+
         public Form1()
         {
             InitializeComponent();
-            textoPDF = new TextoPDF("C:\\Users\\gabi\\Source\\Repos\\gabiwiss\\AppLectorDNI\\AppLectorDNI\\LISTADO-DE-SOCIOS.pdf");
+            textoPDF = new TextoPDF(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LISTADO-DE-SOCIOS.pdf"));
+            timer = new System.Windows.Forms.Timer();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -18,25 +22,32 @@ namespace AppLectorDNI
 
         private void textBox_Enter(object sender, KeyEventArgs e)
         {
+            int i = 0;
             if (e.KeyCode == Keys.Enter)
             {
-
+                timer.Start();
                 if (LectorService.VerificarDNI(sender,textoPDF))
                 {
                     label2.Text = "SOCIO VERIFICADO";
-                    //label2.BackColor = Color.LightSeaGreen;
                     label2.ForeColor = Color.Lime;
                     label2.Show();
-                    /*MessageBox.Show(sender.ToString(), "mensaje correcto")*/
                 }
                 else
                 {
                     label2.Text = "NO ENCONTRADO";
-                    //label2.BackColor = Color.RosyBrown;
                     label2.ForeColor = Color.Red;
                     label2.Show();
-                    /*MessageBox.Show(sender.ToString(), "mensaje incorrecto");*/
                 }
+
+                
+                timer.Interval = 60000; // 10 segundos
+
+                timer.Tick += (sender, e) =>
+                {
+                    label2.Hide();
+                    timer.Stop(); // Detiene el temporizador después de ocultar el mensaje
+                };
+
 
                 textBox1.Text = string.Empty;
             }
